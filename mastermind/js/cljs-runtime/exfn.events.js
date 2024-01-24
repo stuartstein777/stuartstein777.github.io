12d38bca39f0f74b6cd0fde052ef8b8cd87c7378
Stuart Stein <qmstuart@gmail.com>
2024-01-24 17:39:47 +0000

    updating mastermind

--- mastermind/js/cljs-runtime/exfn.events.js
+++ mastermind/js/cljs-runtime/exfn.events.js
@@ -24,10 +24,10 @@ return cljs.core.assoc.cljs$core$IFn$_invoke$arity$3(current_guess,(2),color);
 return cljs.core.assoc.cljs$core$IFn$_invoke$arity$3(current_guess,(1),color);
 }
 });
-re_frame.core.reg_event_db.cljs$core$IFn$_invoke$arity$2(new cljs.core.Keyword(null,"add-guess","add-guess",-115828310),(function (db,p__35700){
-var vec__35701 = p__35700;
-var _ = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__35701,(0),null);
-var color = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__35701,(1),null);
+re_frame.core.reg_event_db.cljs$core$IFn$_invoke$arity$2(new cljs.core.Keyword(null,"add-guess","add-guess",-115828310),(function (db,p__29836){
+var vec__29837 = p__29836;
+var _ = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__29837,(0),null);
+var color = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__29837,(1),null);
 if(cljs.core.truth_((function (){var or__4126__auto__ = (db.cljs$core$IFn$_invoke$arity$1 ? db.cljs$core$IFn$_invoke$arity$1(new cljs.core.Keyword(null,"game-won?","game-won?",-1933899835)) : db.call(null,new cljs.core.Keyword(null,"game-won?","game-won?",-1933899835)));
 if(cljs.core.truth_(or__4126__auto__)){
 return or__4126__auto__;
@@ -40,17 +40,19 @@ return db;
 return cljs.core.assoc.cljs$core$IFn$_invoke$arity$3(db,new cljs.core.Keyword(null,"current-guess","current-guess",-2059201265),exfn.events.add_guess((db.cljs$core$IFn$_invoke$arity$1 ? db.cljs$core$IFn$_invoke$arity$1(new cljs.core.Keyword(null,"current-guess","current-guess",-2059201265)) : db.call(null,new cljs.core.Keyword(null,"current-guess","current-guess",-2059201265))),color));
 }
 }));
-re_frame.core.reg_event_db.cljs$core$IFn$_invoke$arity$2(new cljs.core.Keyword(null,"remove-guess","remove-guess",1131438287),(function (db,p__35704){
-var vec__35705 = p__35704;
-var _ = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__35705,(0),null);
-var position = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__35705,(1),null);
+re_frame.core.reg_event_db.cljs$core$IFn$_invoke$arity$2(new cljs.core.Keyword(null,"remove-guess","remove-guess",1131438287),(function (db,p__29840){
+var vec__29841 = p__29840;
+var _ = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__29841,(0),null);
+var position = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__29841,(1),null);
 return cljs.core.update.cljs$core$IFn$_invoke$arity$5(db,new cljs.core.Keyword(null,"current-guess","current-guess",-2059201265),cljs.core.assoc,position,null);
 }));
 re_frame.core.reg_event_db.cljs$core$IFn$_invoke$arity$2(new cljs.core.Keyword(null,"check-guess","check-guess",-1041834016),(function (db,_){
 var solution = (db.cljs$core$IFn$_invoke$arity$1 ? db.cljs$core$IFn$_invoke$arity$1(new cljs.core.Keyword(null,"solution","solution",-1727231491)) : db.call(null,new cljs.core.Keyword(null,"solution","solution",-1727231491)));
 var guess = (db.cljs$core$IFn$_invoke$arity$1 ? db.cljs$core$IFn$_invoke$arity$1(new cljs.core.Keyword(null,"current-guess","current-guess",-2059201265)) : db.call(null,new cljs.core.Keyword(null,"current-guess","current-guess",-2059201265)));
 var clue = exfn.logic.check_guess(solution,cljs.core.vals(guess));
-return cljs.core.assoc.cljs$core$IFn$_invoke$arity$3(cljs.core.assoc.cljs$core$IFn$_invoke$arity$3(cljs.core.update.cljs$core$IFn$_invoke$arity$4(cljs.core.update.cljs$core$IFn$_invoke$arity$3(cljs.core.assoc.cljs$core$IFn$_invoke$arity$3(db,new cljs.core.Keyword(null,"clues","clues",1918948918),cljs.core.conj.cljs$core$IFn$_invoke$arity$2((db.cljs$core$IFn$_invoke$arity$1 ? db.cljs$core$IFn$_invoke$arity$1(new cljs.core.Keyword(null,"clues","clues",1918948918)) : db.call(null,new cljs.core.Keyword(null,"clues","clues",1918948918))),clue)),new cljs.core.Keyword(null,"current-guess-number","current-guess-number",-93079164),cljs.core.inc),new cljs.core.Keyword(null,"guesses","guesses",272840547),cljs.core.conj,guess),new cljs.core.Keyword(null,"current-guess","current-guess",-2059201265),new cljs.core.PersistentArrayMap(null, 4, [(1),null,(2),null,(3),null,(4),null], null)),new cljs.core.Keyword(null,"game-won?","game-won?",-1933899835),clojure.set.subset_QMARK_(cljs.core.set(solution),cljs.core.set(cljs.core.vals(guess))));
+return cljs.core.assoc.cljs$core$IFn$_invoke$arity$3(cljs.core.assoc.cljs$core$IFn$_invoke$arity$3(cljs.core.update.cljs$core$IFn$_invoke$arity$4(cljs.core.update.cljs$core$IFn$_invoke$arity$3(cljs.core.assoc.cljs$core$IFn$_invoke$arity$3(db,new cljs.core.Keyword(null,"clues","clues",1918948918),cljs.core.conj.cljs$core$IFn$_invoke$arity$2((db.cljs$core$IFn$_invoke$arity$1 ? db.cljs$core$IFn$_invoke$arity$1(new cljs.core.Keyword(null,"clues","clues",1918948918)) : db.call(null,new cljs.core.Keyword(null,"clues","clues",1918948918))),clue)),new cljs.core.Keyword(null,"current-guess-number","current-guess-number",-93079164),cljs.core.inc),new cljs.core.Keyword(null,"guesses","guesses",272840547),cljs.core.conj,guess),new cljs.core.Keyword(null,"current-guess","current-guess",-2059201265),new cljs.core.PersistentArrayMap(null, 4, [(1),null,(2),null,(3),null,(4),null], null)),new cljs.core.Keyword(null,"game-won?","game-won?",-1933899835),cljs.core.every_QMARK_((function (p1__29844_SHARP_){
+return cljs.core._EQ_.cljs$core$IFn$_invoke$arity$2(new cljs.core.Keyword(null,"pos-and-color","pos-and-color",2011772860),p1__29844_SHARP_);
+}),clue));
 }));
 
 //# sourceMappingURL=exfn.events.js.map
--- mastermind/js/cljs-runtime/exfn.logic.js
+++ mastermind/js/cljs-runtime/exfn.logic.js
@@ -4,12 +4,12 @@ exfn.logic.generate_solution = (function exfn$logic$generate_solution(no_colors)
 return cljs.core.keys(cljs.core.take.cljs$core$IFn$_invoke$arity$2(no_colors,cljs.core.shuffle(exfn.logic.colors)));
 });
 exfn.logic.check_guess = (function exfn$logic$check_guess(solution,guess){
-return cljs.core.map.cljs$core$IFn$_invoke$arity$3((function (s,g){
+return cljs.core.shuffle(cljs.core.map.cljs$core$IFn$_invoke$arity$3((function (s,g){
 if(cljs.core._EQ_.cljs$core$IFn$_invoke$arity$2(s,g)){
 return new cljs.core.Keyword(null,"pos-and-color","pos-and-color",2011772860);
 } else {
-if(cljs.core.truth_((function (){var fexpr__34826 = cljs.core.set(solution);
-return (fexpr__34826.cljs$core$IFn$_invoke$arity$1 ? fexpr__34826.cljs$core$IFn$_invoke$arity$1(g) : fexpr__34826.call(null,g));
+if(cljs.core.truth_((function (){var fexpr__29764 = cljs.core.set(solution);
+return (fexpr__29764.cljs$core$IFn$_invoke$arity$1 ? fexpr__29764.cljs$core$IFn$_invoke$arity$1(g) : fexpr__29764.call(null,g));
 })())){
 return new cljs.core.Keyword(null,"color","color",1011675173);
 } else {
@@ -17,21 +17,21 @@ return new cljs.core.Keyword(null,"wrong","wrong",-1945918192);
 
 }
 }
-}),solution,guess);
+}),solution,guess));
 });
 exfn.logic.get_clue_marker = (function exfn$logic$get_clue_marker(clue){
-var pred__34827 = cljs.core._EQ_;
-var expr__34828 = clue;
-if(cljs.core.truth_((pred__34827.cljs$core$IFn$_invoke$arity$2 ? pred__34827.cljs$core$IFn$_invoke$arity$2(new cljs.core.Keyword(null,"pos-and-color","pos-and-color",2011772860),expr__34828) : pred__34827.call(null,new cljs.core.Keyword(null,"pos-and-color","pos-and-color",2011772860),expr__34828)))){
+var pred__29765 = cljs.core._EQ_;
+var expr__29766 = clue;
+if(cljs.core.truth_((pred__29765.cljs$core$IFn$_invoke$arity$2 ? pred__29765.cljs$core$IFn$_invoke$arity$2(new cljs.core.Keyword(null,"pos-and-color","pos-and-color",2011772860),expr__29766) : pred__29765.call(null,new cljs.core.Keyword(null,"pos-and-color","pos-and-color",2011772860),expr__29766)))){
 return new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"i.fas.fa-circle","i.fas.fa-circle",-163893018)], null);
 } else {
-if(cljs.core.truth_((pred__34827.cljs$core$IFn$_invoke$arity$2 ? pred__34827.cljs$core$IFn$_invoke$arity$2(new cljs.core.Keyword(null,"color","color",1011675173),expr__34828) : pred__34827.call(null,new cljs.core.Keyword(null,"color","color",1011675173),expr__34828)))){
+if(cljs.core.truth_((pred__29765.cljs$core$IFn$_invoke$arity$2 ? pred__29765.cljs$core$IFn$_invoke$arity$2(new cljs.core.Keyword(null,"color","color",1011675173),expr__29766) : pred__29765.call(null,new cljs.core.Keyword(null,"color","color",1011675173),expr__29766)))){
 return new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"i.far.fa-circle","i.far.fa-circle",-1409075494)], null);
 } else {
-if(cljs.core.truth_((pred__34827.cljs$core$IFn$_invoke$arity$2 ? pred__34827.cljs$core$IFn$_invoke$arity$2(new cljs.core.Keyword(null,"wrong","wrong",-1945918192),expr__34828) : pred__34827.call(null,new cljs.core.Keyword(null,"wrong","wrong",-1945918192),expr__34828)))){
+if(cljs.core.truth_((pred__29765.cljs$core$IFn$_invoke$arity$2 ? pred__29765.cljs$core$IFn$_invoke$arity$2(new cljs.core.Keyword(null,"wrong","wrong",-1945918192),expr__29766) : pred__29765.call(null,new cljs.core.Keyword(null,"wrong","wrong",-1945918192),expr__29766)))){
 return new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"i.fas.fa-circle.wrong","i.fas.fa-circle.wrong",-246091548)], null);
 } else {
-throw (new Error(["No matching clause: ",cljs.core.str.cljs$core$IFn$_invoke$arity$1(expr__34828)].join('')));
+throw (new Error(["No matching clause: ",cljs.core.str.cljs$core$IFn$_invoke$arity$1(expr__29766)].join('')));
 }
 }
 }
